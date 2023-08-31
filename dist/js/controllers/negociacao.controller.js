785a2fd0e4cc11857cec15d1894a38af0a565144
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-08-31 13:34:51 -0300

    Compiler changes

--- dist/js/controllers/negociacao.controller.js
+++ dist/js/controllers/negociacao.controller.js
@@ -7,12 +7,15 @@ export class NegociacaoController {
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
-    add() {
+    createNegociacao() {
         let data = new Date(this.inputData.value.replace('-', ','));
         let quantidade = parseInt(this.inputQuantidade.value);
         let valor = parseFloat(this.inputValor.value);
         const negociacao = new Negociacao(data, quantidade, valor);
-        this.negociacoes.add(negociacao);
+        return negociacao;
+    }
+    add() {
+        this.negociacoes.add(this.createNegociacao());
         this.limparFormulario();
         console.log(this.negociacoes.read());
         return true;
