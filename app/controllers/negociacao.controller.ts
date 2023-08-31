52825a989b7640acd26f02ff991d149c7f89241e
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-08-31 09:32:50 -0300

    Added a read method to see which elements are present in the products list

--- app/controllers/negociacao.controller.ts
+++ app/controllers/negociacao.controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement
   private inputValor: HTMLInputElement;
+  private negociacoes = new Negociacoes;
 
   constructor() {
     this.inputData = document.querySelector('#data');
@@ -16,8 +18,9 @@ export class NegociacaoController {
     let quantidade = parseInt(this.inputQuantidade.value);
     let valor = parseFloat(this.inputValor.value);
     const negociacao = new Negociacao(data, quantidade, valor)
+    this.negociacoes.add(negociacao)
     this.limparFormulario()
-    console.log(negociacao);
+    console.log(this.negociacoes.read());
     return true
   }
 
