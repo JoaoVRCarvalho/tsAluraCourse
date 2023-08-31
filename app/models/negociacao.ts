8bab05e7c40c09bc7333c92c24cb60038d76d622
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-08-31 13:57:57 -0300

    Redifined the read only prop on the date variable truly protect it

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,25 +1,18 @@
 export class Negociacao {
 
-
     constructor(
-        private _data: Date,
-        private _quantidade: number,
-        private _valor: number,        
+      private _data: Date, // no caso de date, ela não pdoe ficar como read only, pois ela não um tipo primitivo.
+      public readonly quantidade: number,
+      public readonly valor: number,        
     ) {}
 
     get data(): Date {
-        return this._data;
-    }
-
-    get quantidade(): number {
-        return this._quantidade;
-    }
-
-    get valor(): number {
-        return this._valor;
+      // como date não é um tipo primitivo, é feita está gambiarra para que se evite a mutação do valor por meio de métdodos 
+      let date = new Date(this._data.getTime())
+      return date;
     }
 
     get volume(): number {
-        return this._quantidade * this._valor;
+      return this.quantidade * this.valor;
     }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,19 +1,16 @@
 export class Negociacao {
-    constructor(_data, _quantidade, _valor) {
+    constructor(_data, // no caso de date, ela não pdoe ficar como read only, pois ela não um tipo primitivo.
+    quantidade, valor) {
         this._data = _data;
-        this._quantidade = _quantidade;
-        this._valor = _valor;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
+        // como date não é um tipo primitivo, é feita está gambiarra para que se evite a mutação do valor por meio de métdodos 
+        let date = new Date(this._data.getTime());
+        return date;
     }
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
