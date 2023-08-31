84bdfc51588155d04197f279cb5d4b2b0b88a128
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-08-31 09:41:52 -0300

    removed the read only via ... and added it using ts readonly in negociacoes.ts model

--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -6,6 +6,6 @@ export class Negociacoes {
         this._negociacoes.push(negociacao);
     }
     read() {
-        return [...this._negociacoes]; // Para que não seja retornado a propriedade privada que deve ser imutavel, retorna-se um outro array que é uma copia ao array de negociacoes. 
+        return this._negociacoes;
     }
 }
