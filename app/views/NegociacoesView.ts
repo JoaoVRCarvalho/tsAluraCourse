500a5ba5d846a990eed005ee6403a9f56be0f001
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-09-01 19:26:23 -0300

    Created view for the dynamic created table using the items in list

---
+++ app/views/NegociacoesView.ts
@@ -0,0 +1,10 @@
+export class NegociacoesView {
+  template(): String {
+    return `
+      <table class="table table-hover table-bordered">
+        <thead></thead>
+      </table>
+    `    
+  }
+
+}
