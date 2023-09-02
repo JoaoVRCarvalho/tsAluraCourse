5aec9a2068d3f28e618be7f295773e02125b3110
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-09-02 10:05:25 -0300

    View for the dynamic created table which takes array negociacoes as parameter to render it in the table

--- app/views/NegociacoesView.ts
+++ app/views/NegociacoesView.ts
@@ -1,10 +1,42 @@
+import { Negociacao } from "../models/negociacao";
+import { Negociacoes } from "../models/negociacoes";
+
 export class NegociacoesView {
-  template(): String {
+  private elemento: HTMLElement;
+
+  constructor(seletor: string) {
+    this.elemento = document.querySelector(seletor);
+  }
+
+  template(model: Negociacoes): string {
     return `
       <table class="table table-hover table-bordered">
-        <thead></thead>
+        <thead>
+          <tr>
+            <th>DATA</th>
+            <th>QUANTIDADE</th>
+            <th>VALOR</th>
+            </tr>
+        </thead>
+        <tbody>
+          ${model.read().map(negociacao => this.negociacaoRow(negociacao)).join('')}
+        </tbody>
       </table>
     `    
+  }
+
+  update(model: Negociacoes): void {
+    this.elemento.innerHTML = this.template(model);
+  }
+
+  negociacaoRow(negociacao: Negociacao) {
+    return `
+      <tr>
+        <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+        <td>${negociacao.quantidade}</td>
+        <td>${negociacao.valor}</td>
+      </tr>
+    `
   }
 
 }
