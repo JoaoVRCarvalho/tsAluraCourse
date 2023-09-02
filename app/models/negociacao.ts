e0b8912911052225639da667a1ad40a51b79a706
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-09-02 10:09:17 -0300

    Corrected indentation

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,18 +1,18 @@
 export class Negociacao {
 
-    constructor(
-      private _data: Date, // no caso de date, ela não pdoe ficar como read only, pois ela não um tipo primitivo.
-      public readonly quantidade: number,
-      public readonly valor: number,        
-    ) {}
+  constructor(
+    private _data: Date, // no caso de date, ela não pdoe ficar como read only, pois ela não um tipo primitivo.
+    public readonly quantidade: number,
+    public readonly valor: number,        
+  ) {}
 
-    get data(): Date {
-      // como date não é um tipo primitivo, é feita está gambiarra para que se evite a mutação do valor por meio de métdodos 
-      let date = new Date(this._data.getTime())
-      return date;
-    }
+  get data(): Date {
+    // como date não é um tipo primitivo, é feita está gambiarra para que se evite a mutação do valor por meio de métdodos 
+    let date = new Date(this._data.getTime())
+    return date
+  }
 
-    get volume(): number {
-      return this.quantidade * this.valor;
-    }
+  get volume(): number {
+    return this.quantidade * this.valor;
+  }
 }
