a68e367d7ffb87b36dc6ca4e1656c5b2e9bc839e
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-09-06 13:54:46 -0300

    Added new prop that controls if the template will be secure

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,12 +1,17 @@
 export abstract class View<T> {
 
   protected element: HTMLElement;
+  protected escape = false;
 
-  constructor(seletor: string) {
+  constructor(seletor: string, escape?: boolean) {
     this.element = document.querySelector(seletor);
   }
 
   update(model: T): void {
+    let template = this.template(model);
+    if (this.escape) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+    }
     this.element.innerHTML = this.template(model);
   }
 
