108dc59109a112aafd7918506dbb84e1f75afe4d
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-09-06 13:56:06 -0300

    Added prop to control template security for js script insertion

--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,8 +1,13 @@
 export class View {
-    constructor(seletor) {
+    constructor(seletor, escape) {
+        this.escape = false;
         this.element = document.querySelector(seletor);
     }
     update(model) {
+        let template = this.template(model);
+        if (this.escape) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
         this.element.innerHTML = this.template(model);
     }
 }
