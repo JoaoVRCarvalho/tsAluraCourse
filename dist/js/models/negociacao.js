48381ddd31003ec295bd856b769d3544fa35863e
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-10-10 12:47:21 -0300

    compiler

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,12 +1,10 @@
 export class Negociacao {
-    constructor(_data, // no caso de date, ela não pdoe ficar como read only, pois ela não um tipo primitivo.
-    quantidade, valor) {
+    constructor(_data, quantidade, valor) {
         this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
     }
     get data() {
-        // como date não é um tipo primitivo, é feita está gambiarra para que se evite a mutação do valor por meio de métdodos 
         let date = new Date(this._data.getTime());
         return date;
     }
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,6 +1,9 @@
 export class View {
     constructor(seletor, escape) {
         this.escape = false;
+        if (!document.querySelector(seletor)) {
+            throw `o seletor: ${seletor} não está presente no DOM`;
+        }
         this.element = document.querySelector(seletor);
     }
     update(model) {
