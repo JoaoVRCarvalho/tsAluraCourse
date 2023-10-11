966e92137e4b36cfe17d702d05e96e87d9aff0b6
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-10-11 12:44:25 -0300

    created a decorator to log performance

---
+++ app/src/decorators/performance.log.ts
@@ -0,0 +1,19 @@
+export function logPerformance() {
+  return function(
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originMethod = descriptor.value;
+
+    descriptor.value = function(...args: Array<any>) {
+      const time1 = performance.now();
+      const funcReturn = originMethod.apply(this, args);
+      const time2 = performance.now();
+      console.log(`tempo de execução do método [${propertyKey}] ${(time2 - time1)/1000} segundos`);
+      funcReturn;
+    }
+
+    return descriptor;
+  }
+}
