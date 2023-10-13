53a1b926491c8a7b2d3dee1ac71193efda790d7f
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-10-13 09:40:37 -0300

    Created decorator for the escape method in the view children

---
+++ app/src/decorators/escape.ts
@@ -0,0 +1,19 @@
+export function escape(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
+  const originFn = descriptor.value;
+
+  descriptor.value = function(...args: Array<any>) {
+    let fnReturn = originFn.apply(this, args);
+    if (typeof fnReturn === 'string') {
+      console.log('escaping...');
+      fnReturn = fnReturn
+        .replace(/<script>[\s\S]*?<\/script>/, '');
+    }
+    return fnReturn;
+  }
+
+  return descriptor;
+}
