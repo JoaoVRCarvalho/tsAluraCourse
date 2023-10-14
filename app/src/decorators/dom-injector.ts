0ca028f2e7cc9debe36dbfa490a398da1a2be340
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-10-14 19:33:45 -0300

    Defined decorator for injection on the DOM object

---
+++ app/src/decorators/dom-injector.ts
@@ -0,0 +1,25 @@
+export function domInjector(seletor: string) {
+
+  return function(
+    target: any,
+    propertyKey: string,
+  ) {
+
+    let element: HTMLElement;
+    const getter = function() {
+      if (!element) {
+        console.log('buscando elemento')
+        element = document.querySelector(seletor) as HTMLElement;
+        console.log(`element: ${element} injected at ${propertyKey}`);
+      }
+
+      return element;
+    }
+
+    Object.defineProperty(
+      target, 
+      propertyKey, 
+      { get: getter }
+    )
+  }
+}
